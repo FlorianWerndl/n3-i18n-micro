82cf80ca4396de5eb33bb211e81ccbe4511c92b6
flowern <fwerndl@gmail.com>
2024-10-24 16:14:12 +0200

    refactor pages creation (#1)
    * refactor(no-jira): refactor page creation for router
    
    * refactor(no-jira): refactor pages creation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 import availableLanguages from './app/locales/availableLanguages';
-import pages from './app/locales/pages';
+import { createPages } from './app/locales/pages';
 
+const pages = createPages();
+// console.log('pages :>> ', pages);
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
@@ -24,24 +26,14 @@ export default defineNuxtConfig({
   },
 
   i18n: {
-    disableWatcher: true,
-    locales: availableLanguages,
     autoDetectLanguage: true,
     autoDetectPath: '/',
-    includeDefaultLocaleRoute: true,
-    globalLocaleRoutes: pages,
-    // globalLocaleRoutes: {
-    //   'page': {
-    //     en: '/page',
-    //     de: '/seite',
-    //   },
-    //   'page-identifier': {
-    //     en: '/page/:identifier()',
-    //     de: '/seite/:identifier()',
-    //   },
-    // },
     defaultLocale: 'de',
-    translationDir: './app/locales',
+    disableWatcher: true,
+    globalLocaleRoutes: pages,
+    includeDefaultLocaleRoute: true,
+    locales: availableLanguages,
     meta: true,
+    translationDir: './app/locales',
   },
 });
